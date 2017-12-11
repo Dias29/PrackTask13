e8724c39912c967a4a1be7a39bd28d0116b5dae3
УринбасаровД <@A-305-10>
2017-12-11 17:58:47 +0600

    first

---
+++ PracktTask13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracktTask13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList numbers = new ArrayList(10);
+            for (int index = 0; index < numbers.Capacity; index++)
+            {
+                numbers.Add(index);
+                Console.WriteLine(numbers[index]);
+
+            }
+            for (int del = 0; del < numbers.Capacity; del++)
+            {
+                if ((int)numbers[del] % 2 != 0)
+                {
+                    // Console.SetCursorPosition
+                    Console.WriteLine("_" + numbers[del]);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SecondTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>(5);
+            Random rnd = new Random();
+            int sum = 0;
+
+            for(int i = 0; i<numbers.Capacity; i++)
+            {
+                int num = rnd.Next(0, 10);
+                numbers.Add(num);
+                Console.WriteLine(numbers[i]);
+                
+                sum = sum + numbers[i];
+
+                
+            }
+
+            Console.WriteLine("\nSum= " + sum);
+
+            for (int i = 0; i < numbers.Capacity; i++)
+            {
+                int middleAriphmetic = sum / numbers.Capacity;
+                if (numbers[i] > middleAriphmetic)
+                {
+                    Console.WriteLine("\nNum= " + numbers[i]);
+                }
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
